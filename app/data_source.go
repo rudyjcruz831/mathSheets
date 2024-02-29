eb3630d79b6329cd25232cb1ecbf1337c4c94325
rudyjcruz831 <rudyjcruz831@gmail.com>
2024-02-29 11:05:25 -0800

    just testing repo for now

---
+++ app/data_source.go
@@ -0,0 +1,14 @@
+package app
+
+// import "gorm.io/gorm"
+
+// type dataSource struct {
+// 	DB *gorm.DB
+// 	//RedisClient *redis.Client
+// 	//StorageClient *storage.Client
+
+// }
+
+// func initDS(*dataSource, error) {
+
+// }
